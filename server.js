873e5145f59dbd278c082048335cf3f3f262df8b
zestuus <a.kushka.30@gmail.com>
2020-02-24 00:52:54 +0200

    change port to deploy on heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
-app.listen(3000,()=>console.log('listening on port 3000...'))
+app.listen(port,()=>console.log(`listening on port ${port}...`))
 
 app.use(express.static("public"));
