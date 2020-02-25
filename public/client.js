fc34691caecabaeac0aae60aedf609e4d9374d72
zestuus <a.kushka.30@gmail.com>
2020-02-25 10:00:04 +0200

    add uploading files

--- public/client.js
+++ public/client.js
@@ -1,3 +1,9 @@
-document.getElementById("upload").onchange = function() {
-    document.getElementById("open").submit();
-};
+document.getElementById("file").onchange = function() {
+    const file = $('#file')[0].files[0];
+    const reader = new FileReader();
+    reader.onload = function(){
+    	document.getElementById("filename").value = file.name;
+    	document.getElementById("text").innerHTML = this.result;
+    }
+    reader.readAsText(file);
+}
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const fs = require("fs");
 
 app.listen(port,()=>console.log(`listening on port ${port}...`))
 
 app.use(express.static("public"));
+
+app.post("/upload", upload.single('file'), (req, res)=> {
+	fs.readFile(req.file.path, 'utf8', function(err, contents) {
+	    const filename = req.file.originalname;
+	    fs.unlinkSync(req.file.path)
+	    const data = {
+	    	filename,
+	    	contents
+	    }
+	    res.send(data)
+	});
+})
