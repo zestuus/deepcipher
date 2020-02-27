cc07b04e1b65c78e570eb6d8603ced7bd3010f4f
zestuus <a.kushka.30@gmail.com>
2020-02-27 16:53:25 +0200

    add encrypting and decrypting

--- public/client.js
+++ public/client.js
@@ -1,9 +1,74 @@
+const ukrainian_alphabet = [' ', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я']
+const english_alphabet = [' ', 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+// console.log(ukrainian_alphabet.length) === 34
+// console.log(english_alphabet.length) === 27
+
+class Cipher {
+  static encrypt(input, shift) { throw Error('Abstract method!')}
+  static decrypt(input, shift) { throw Error('Abstract method!')}
+}
+
+class Caesar extends Cipher{
+  static encryptEnCharacter(char, shift) {
+    let index = ukrainian_alphabet.indexOf(char);
+    return english_alphabet[(index + shift)%27]
+  }
+  static encryptUkCharacter(char, shift) {
+    const index = ukrainian_alphabet.indexOf(char);
+    const shiftedIndex = index + shift;
+    const encryptedIndex = shiftedIndex > 0 ?(shiftedIndex)%34: (shiftedIndex+34)%34;
+
+    return ukrainian_alphabet[encryptedIndex];
+  }
+  static getType(char) {
+    let charCode = char.charCodeAt();
+    if(97<=charCode && charCode<=122)
+      return 'en';
+    if(char === ' ' || char === '\n')
+      return ' ';
+    if(ukrainian_alphabet.indexOf(char) !==-1)
+      return 'uk';
+    //else
+    return 'symbol'
+  }
+  static encrypt(input, shift) { 
+    console.log('Encryption has been started');
+    let text = input.value.toLowerCase();
+    let encoded = '';
+    let lang = '';
+    for (let char of text) {
+      const type = Caesar.getType(char);
+      if(type==='en') {
+        lang = 'en';
+        encoded+= Caesar.encryptEnCharacter(char, shift);
+      }
+      else if(type==='uk') {
+        lang = 'uk';
+        encoded+= Caesar.encryptUkCharacter(char, shift);
+      }
+      else if(type===' ') {
+        if(char === '\n')
+          char = ' '
+        if(lang === 'en')
+          encoded+= Caesar.encryptEnCharacter(char, shift);
+        else
+          encoded+= Caesar.encryptUkCharacter(char, shift);
+      }
+    }
+    input.value = encoded;
+  }
+  static decrypt(input, shift) { 
+    Caesar.encrypt(input,-shift)
+  }
+}
+
 document.getElementById("file").onchange = function() {
     const file = $('#file')[0].files[0];
     const reader = new FileReader();
     reader.onload = function(){
     	document.getElementById("filename").value = file.name;
-    	document.getElementById("text").innerHTML = this.result;
+    	document.getElementById("content").innerHTML = this.result;
     }
     reader.readAsText(file);
 }
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const fs = require("fs");
+const port = process.env.PORT || 3000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
-app.listen(port,()=>console.log(`listening on port ${port}...`))
+app.listen(port,()=>console.log(`listening on port ${port}...`));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post("/upload", upload.single('file'), (req, res)=> {
-	fs.readFile(req.file.path, 'utf8', function(err, contents) {
-	    const filename = req.file.originalname;
-	    fs.unlinkSync(req.file.path)
-	    const data = {
-	    	filename,
-	    	contents
-	    }
-	    res.send(data)
-	});
-})
+app.post('/save', (req, res)=> {
+  const content = req.body.content;
+  const name = req.body.filename;
+
+  const hasExtention = name.indexOf(".txt") === -1;
+  const filename = hasExtention ? name + ".txt" : name;
+  const filePath = __dirname + "/to_be_sent/"+ filename
+
+	fs.writeFile(filePath, content,()=>{
+    console.log('file is created');
+    res.download(filePath, () => 
+      fs.unlink(filePath, () => 
+        console.log("file is downloaded and removed")
+    ));
+  });
+});
